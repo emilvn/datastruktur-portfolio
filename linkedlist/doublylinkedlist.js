8ecd6d236a86d45befe0ddabc238b9ef36e697ab
emilvn <emilvnielsen@hotmail.com>
2024-09-09 20:55:49 +0200

    doubly linked list done

--- linkedlist/doublylinkedlist.js
+++ linkedlist/doublylinkedlist.js
@@ -1,52 +1,64 @@
 export default class DoublyLinkedList {
+    /**
+     * First element of list
+     */
     head = null;
+    /**
+     * Last element of list
+     */
     tail = null;
 
+    /**
+     * @param  {...Node} nodes list of nodes to store in DoublyLinkedList on construction. Will be stored in the same order as args.
+     */
     constructor(...nodes) {
-        this.head = nodes[0];
-        this.tail = nodes[0];
-        for (let i = 1; i < nodes.length; i++) {
-            const current = nodes[i]
-            current.next = this.head;
-            this.head.prev = current;
-            this.head = current;
+        // Insert nodes starting with tail to match indeces in linked list to the order of the args
+        if (nodes.length > 0) {   
+            this.head = nodes[0];
+            this.tail = nodes[0];
+            for (let i = 1; i < nodes.length; i++) {
+                const current = nodes[i]
+                current.prev = this.tail;
+                this.tail.next = current;
+                this.tail = current;
+            }
         }
     }
 
+    /**
+     * Creates new node with data and adds it as the last node of the list
+     * @param {any} data data to be stored in node
+     */
     addLast(data) {
         const node = new Node(data);
-        if (!this.tail) {
-            this.head = node;
-            this.tail = node;
-            return;
-        }
-
-        node.prev = this.tail;
-        node.prev.next = node;
-        this.tail = node;
+        this.addNodeLast(node);
     }
 
+    /**
+     * Creates new node with data and adds it as the first node of the list
+     * @param {any} data data to be stored in node
+     */
     addFirst(data) {
         const node = new Node(data);
-        if (!this.head) {
-            this.head = node;
-            this.tail = node;
-            return;
-        }
-
-        node.next = this.head;
-        node.next.prev = node;
-        this.head = node;
+        this.addNodeFirst(node);
     }
 
+    /**
+     * Gets data from node at index
+     * @param {number} index index of node to get data from
+     * @returns {any} data of node at index
+     */
     get(index) {
-        let node = this.head;
-        for (let i = 0; i < index; i++) {
-            node = node?.next;
-        }
-        return node ?? null;
+        let node = this.nodeAt(index);
+        return node?.data; // undefined if index out of bounds
     }
 
+
+    /**
+     * Gets index of node with data
+     * @param {any} data data of node to find index of
+     * @returns {number} index of node with data, or -1 if not found
+     */
     indexOf(data) {
         let node = this.head;
         let i = 0;
@@ -60,93 +72,258 @@ export default class DoublyLinkedList {
         return -1; // not found
     }
 
+    /**
+     * Creates new node with data and inserts it after the index
+     * @param {number} index index of node to insert data after
+     * @param {*} data data of node to insert
+     */
     insertAfter(index, data) {
-        
+        const newNode = new Node(data);
+        const leftNode = this.nodeAt(index);
+        if (!leftNode) return; // index out of bounds
+        this.insertAfterNode(newNode, leftNode);
     }
 
+    /**
+     * Creates new node with data and inserts it before the index
+     * @param {number} index index of node to insert data before
+     * @param {*} data data of node to insert
+     */
     insertBefore(index, data) {
-
+        const newNode = new Node(data);
+        const rightNode = this.nodeAt(index);
+        if (!rightNode) return; // index out of bounds
+        this.insertBeforeNode(newNode, rightNode);
     }
 
+    /**
+     * Gets data of first node/head of list
+     * @returns {any | undefined} data of first node/head of list
+     */
     first() {
-        return this.head;
+        return this.head?.data;
     }
 
+    /**
+     * Gets data of last node/tail of list
+     * @returns {any | undefined} data of last node/tail of list
+     */
     last() {
-        return this.tail;
+        return this.tail?.data;
     }
 
+    /**
+     * Finds node by data and removes it from the list
+     * @param {any} data data of node to remove 
+     */
     remove(data) {
-
+        let node = this.head;
+        let found = false;
+        while (!!node) {
+            if (node.data === data) {
+                found = true;
+                break;
+            }
+            node = node.next;
+        }
+        if (found) {
+            this.removeNode(node);
+        }
     }
 
+    /**
+     * Removes node at index
+     * @param {number} index index of node to remove 
+     */
     removeIndex(index) {
-
+        let node = this.nodeAt(index);
+        if (!node) return;
+        this.removeNode(node);
     }
 
+    /**
+     * Removes first node/head of list
+     */
     removeFirst() {
+        if (this.head === this.tail) {
+            this.clear();
+            return;
+        }
 
+        this.head = this.head.next;
+        this.head.prev = null;
     }
 
+    /**
+     * Removes last node/tail of list
+     */
     removeLast() {
+        if (this.head === this.tail) {
+            this.clear();
+            return;
+        }
 
+        this.tail = this.tail.prev;
+        this.tail.next = null;
     }
 
     //NODE SPECIFIC OPERATIONS
 
+    /**
+     * Adds node at the end of the list
+     * @param {Node} node node to add 
+     */
     addNodeLast(node) {
+        if (!this.tail) {
+            this.clear();
+            return;
+        }
 
+        node.prev = this.tail;
+        node.prev.next = node;
+        this.tail = node;
     }
 
+    /**
+     * Adds node at the start of the list
+     * @param {Node} node node to add 
+     */
     addNodeFirst(node) {
+        if (!this.head) {
+            this.clear();
+            return;
+        }
 
+        node.next = this.head;
+        node.next.prev = node;
+        this.head = node;
     }
 
+    /**
+     * Inserts new node after an existing node
+     * @param {Node} newNode node to insert
+     * @param {Node} existingNode node to insert new node after
+     */
     insertAfterNode(newNode, existingNode) {
+        newNode.next = existingNode.next
+        newNode.prev = existingNode
 
+        if (!existingNode.next) {   
+            this.tail = newNode;
+        } else {
+            existingNode.next.prev = newNode;
+        }
+        existingNode.next = newNode;
     }
 
+    /**
+     * Inserts new node before an existing node
+     * @param {Node} newNode node to insert
+     * @param {Node} existingNode node to insert new node before
+     */
     insertBeforeNode(newNode, existingNode) {
+        newNode.prev = existingNode.prev
+        newNode.next = existingNode
 
+        if (!existingNode.prev) {   
+            this.head = newNode;
+        } else {
+            existingNode.prev.next = newNode
+        }
+        existingNode.prev = newNode
     }
 
+    /**
+     * Removes node from list
+     * @param {Node} node node to remove 
+     */
     removeNode(node) {
-
+        const prev = node.prev;
+        const next = node.next;
+        if (!prev) {
+            this.head = next;
+        } else {
+            prev.next = next;
+        }
+        if (!next) {
+            this.tail = prev;
+        } else {
+            next.prev = prev;
+        }
     }
 
+    /**
+     * Gets node at given index
+     * @param {number} index index of node to get 
+     * @returns {Node | null} node at index or null if index out of bounds
+     */
     nodeAt(index) {
-
+        let node = this.head;
+        for (let i = 0; i < index; i++){
+            node = node?.next // undefined if index out of bounds
+        }
+        return node ?? null; // null if index out of bounds
     }
 
+    /**
+     * Swaps the data of 2 nodes
+     * @param {Node} nodeA first node to swap
+     * @param {Node} nodeB second node to swap
+     */
     swapNodes(nodeA, nodeB) {
-
+        // We only have to swap the data
+        const dataHolder = nodeA.data;
+        nodeA.data = nodeB.data;
+        nodeB.data = dataHolder;
     }
 
     // Helpers
 
+    /**
+     * Clears list
+     */
     clear() {
-        
+        this.head = null;
+        this.tail = null;
     }
 
+    /**
+     * Gets size of list
+     * @returns {number} size of list
+     */
     size() {
-        
+        let node = this.head;
+        let size = 0;
+        while (!!node) {
+            size++
+            node = node.next;
+        }
+        return size;
     }
 
+    /**
+     * Prints list to console, primarily for debugging
+     */
     dumpList() {
         let node = this.head;
-        let output = node.data;
+        let output = node?.data;
         while (!!node) {
             if (node !== this.head) {   
                 output += " <-> " + node.data;
             }
             node = node.next
         }
-        console.log(output);
+        console.log("Head: " + this.head?.data);
+        console.log("Tail: " + this.tail?.data);
+        console.log("List: " + output);
         
     }
 
 }
 
+/**
+ * Class for DoubleLinkedList nodes
+ */
 class Node {
     prev = null;
     next = null;
--- linkedlist/test_doublylinkedlist.js
+++ linkedlist/test_doublylinkedlist.js
@@ -12,33 +12,188 @@ class Node {
 
 
 
-const a = new Node("A");
-const b = new Node("B");
-const c = new Node("C");
+let a = new Node("A");
+let b = new Node("B");
+let c = new Node("C");
+let d = new Node("D");
 
-const list = new DoublyLinkedList(a, b, c);
+function resetNodes() {
+    a = new Node("A");
+    b = new Node("B");
+    c = new Node("C");
+    d = new Node("D");
+}
 
-// // Test dumplist
-// list.dumpList();
+let list = new DoublyLinkedList(a, b, c);
 
-// // Test addFirst
-// list.dumpList();
-// list.addFirst("D")
-// list.dumpList();
+// Test addFirst
+console.log("Test addFirst");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.addFirst("D")
+list.dumpList();
+console.log("---------------------------");
 
-// // Test addLast
-// list.dumpList();
-// list.addLast("D")
-// list.dumpList();
+// Test addLast
+console.log("Test addLast");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.addLast("D")
+list.dumpList();
+console.log("---------------------------");
 
-// // Test get
-// console.log(list.get(0)?.data) // should be 'C'
-// console.log(list.get(1)?.data) // should be 'B'
-// console.log(list.get(2)?.data) // should be 'A'
-// console.log(list.get(3)?.data) // should be undefined (node is null)
+// Test get
+console.log("Test get");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+console.log(list.get(0)) // should be 'A'
+console.log(list.get(1)) // should be 'B'
+console.log(list.get(2)) // should be 'C'
+console.log(list.get(3)) // should be undefined (node is null)
+console.log("---------------------------");
 
 // Test indexOf
-console.log(list.indexOf("A")); // should be 2
+console.log("Test indexOf");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+console.log(list.indexOf("A")); // should be 0
 console.log(list.indexOf("B")); // should be 1
-console.log(list.indexOf("C")); // should be 0
+console.log(list.indexOf("C")); // should be 2
 console.log(list.indexOf("D")); // should be -1
+console.log("---------------------------");
+
+// Test insertAfter
+console.log("Test insertAfter");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.insertAfter(2, "D");
+list.dumpList();
+console.log("---------------------------");
+
+// Test insertBefore
+console.log("Test insertBefore");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.insertBefore(0, "D");
+list.dumpList();
+console.log("---------------------------");
+
+// Test Remove
+console.log("Test remove");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.remove("C");
+list.dumpList();
+console.log("---------------------------");
+
+// Test Remove index
+console.log("Test removeIndex");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.removeIndex(2);
+list.dumpList();
+console.log("---------------------------");
+
+// Test Remove first
+console.log("Test removeFirst");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.removeFirst();
+list.dumpList();
+console.log("---------------------------");
+
+// Test Remove last
+console.log("Test removeLast");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.removeLast();
+list.dumpList();
+console.log("---------------------------");
+
+// Test addNodeFirst
+console.log("Test addNodeFirst");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.addNodeFirst(d)
+list.dumpList();
+console.log("---------------------------");
+
+// Test addNodeLast
+console.log("Test addNodeLast");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.addNodeLast(d)
+list.dumpList();
+console.log("---------------------------");
+
+// Test insertAfterNode
+console.log("Test insertAfterNode");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.insertAfterNode(d, b);
+list.dumpList();
+console.log("---------------------------");
+
+// Test insertBeforeNode
+console.log("Test insertBeforeNode");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.insertBeforeNode(d, a);
+list.dumpList();
+console.log("---------------------------");
+
+// Test removeNode
+console.log("Test removeNode");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.removeNode(b);
+list.dumpList();
+console.log("---------------------------");
+
+// Test nodeAt
+console.log("Test nodeAt");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+console.log(list.nodeAt(0)?.data); // should be "A"
+console.log(list.nodeAt(1)?.data); // should be "B"
+console.log(list.nodeAt(2)?.data); // should be "C"
+console.log(list.nodeAt(3)?.data); // should be "undefined"
+console.log("---------------------------");
+
+// Test swapNodes
+console.log("Test swapNodes");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.swapNodes(a, c);
+list.dumpList();
+console.log("---------------------------");
+
+// Test clear
+console.log("Test clear");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+list.dumpList();
+list.clear();
+list.dumpList();
+console.log("---------------------------");
+
+// Test size
+console.log("Test size");
+resetNodes();
+list = new DoublyLinkedList(a, b, c);
+console.log(list.size()) // should be 3
+console.log("---------------------------");
